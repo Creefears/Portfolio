f06e3163c4445df891bb17529d42f0651cef807a
Victor Jacob <36567681+Creefears@users.noreply.github.com>
2025-05-23 11:16:00 +0200

    Add useCallback to React imports

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useSwipeable } from 'react-swipeable';
 import { motion, AnimatePresence } from 'framer-motion';
